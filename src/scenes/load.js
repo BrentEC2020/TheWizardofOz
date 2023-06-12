94ccae7463125595dc8fe4ff88017fef32a4528b
Brent Chou <brechou@ucsc.edu>
2023-06-12 15:22:25 -0700

    added new storm sound

--- src/scenes/load.js
+++ src/scenes/load.js
@@ -32,7 +32,7 @@ class Load extends Phaser.Scene{
         this.load.image("ybroad", "yellowbrickroad.png");
 
         this.load.audio('throw', "throw.mp3");
-        this.load.audio('storm', "storm.wav");
+        this.load.audio('storm', "storm.mp3");
         this.load.audio('tornadocrash', "tornadocrash.mp3");
         this.load.audio('switch', "lightswitch.mp3");
         this.load.audio('song', "song.mp3");
