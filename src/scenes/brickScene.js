d0faaf262390533fcb743ee768e11ebe624cb2c1
Brent Chou <brechou@ucsc.edu>
2023-06-12 16:30:54 -0700

    adjusting ybroad bounds

--- src/scenes/brickScene.js
+++ src/scenes/brickScene.js
@@ -5,7 +5,7 @@ class BrickScene extends Phaser.Scene {
     }
   
     create() {
-      this.ybroad = this.add.tileSprite(0,0, 650, 425, 'ybroad').setOrigin(0,0);
+      this.ybroad = this.add.tileSprite(0,0, game.config.width * 2, game.config.height, 'ybroad').setOrigin(0,0);
       this.justroad = this.physics.add.sprite(0,game.config.height/3 - 8, "justroad").setOrigin(0,0);
       keyW = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
       keyA = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
