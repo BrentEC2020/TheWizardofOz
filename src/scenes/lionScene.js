d124cad3e705f04a25a76feb8ba9b138ede036b9
Arthur Lin <arslin@ucsc.edu>
2023-06-12 23:03:54 -0700

    lion move and lion attacking implemented

--- src/scenes/lionScene.js
+++ src/scenes/lionScene.js
@@ -19,8 +19,20 @@ class LionScene extends Phaser.Scene {
       .setScale(2);
     this.player.setCollideWorldBounds(true);
     this.lion = this.physics.add.sprite(600, 350, "lion", 0).setScale(2);
-    this.lion.flipX = true;
-    // this.physics.add.collider(this.player, this.scarecrow);
+    this.lion.flipX = true; 
+    this.claws = this.add.group();
+    this.hands = this.add.group();
+
+    this.playerSlap();
+
+    this.lionattacking = this.time.addEvent({
+      delay: 1000, 
+      callback: this.lionattack,
+      callbackScope: this,
+      loop: true
+    });
+
+
   }
 
   update() {
@@ -42,6 +54,11 @@ class LionScene extends Phaser.Scene {
       this.VEL * this.direction.y
     );
     // follow script for player
+    this.friendsfollow();
+    this.lionmove(this.player.body.velocity.x,this.player.body.velocity.y);
+  }
+
+  friendsfollow() {
     if (this.player.body.velocity.x != 0 || this.player.body.velocity.y != 0) {
       this.physics.moveToObject(this.scarecrow, this.player, 70);
       this.physics.moveToObject(this.tinman, this.scarecrow, 60);
@@ -52,9 +69,56 @@ class LionScene extends Phaser.Scene {
       this.tinman.body.velocity.x = 0;
       this.tinman.body.velocity.y = 0;
     }
-
   }
-  lionmove() {
 
+  playerSlap() {
+    this.input.on("pointerdown", (pointer) => {
+      this.sound.play("throw", {volume: 0.1})
+      let bspeed = 100;
+      let hand = this.add
+        .sprite(
+          this.player.x,
+          this.player.y,
+          "bird",
+          0
+        )
+        .setDepth(9).setScale(2);
+
+      this.physics.add.existing(hand);
+
+      let vector = new Phaser.Math.Vector2(
+        pointer.worldX - this.player.x,
+        pointer.worldY - this.player.y
+      );
+      vector.setLength(bspeed);
+      this.hands.add(hand);
+      hand.body.setVelocity(vector.x, vector.y);
+      this.time.delayedCall(100, () => {
+        hand.destroy();
+      });
+    });   
   }
+
+  lionmove(speedx, speedy) {
+    if (speedx != 0 || speedy != 0) {
+      this.physics.moveToObject(this.lion, this.player, 20);
+    }
+    else {
+      this.lion.body.velocity.x = 0;
+      this.lion.body.velocity.y = 0;
+    }
+  }
+
+  lionattack() {
+    if (this.player.body.velocity.x != 0 || this.player.body.velocity.y != 0) {
+    let vector = new Phaser.Math.Vector2(this.player.x-this.lion.x,this.player.y-this.lion.y);
+    this.slash = this.physics.add.sprite(this.lion.x, this.lion.y, "claw", 0).setDepth(9);
+    
+    vector.setLength(100);
+    this.claws.add(this.slash);
+    this.slash.setVelocity(vector.x,vector.y);
+    }
+  }
+
 }
+
